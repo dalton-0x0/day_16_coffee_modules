e691643e53f76e17e4959ddcbca6e7c49b4a2e0e
Dalton Okechukwu <dalton.0x0@gmail.com>
2024-02-26 21:34:34 -0700

    adds model for menu

---
+++ coffee_machine/menu/menu.py
@@ -0,0 +1,34 @@
+from .menu_item import MenuItem
+
+
+class Menu:
+    """Models the Menu with drinks."""
+
+    def __init__(self):
+        self.menu = [
+            MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5),
+            MenuItem(name="espresso", water=50, milk=0, coffee=18, cost=1.5),
+            MenuItem(name="cappuccino", water=250, milk=50, coffee=24, cost=3),
+        ]
+
+    def get_items(self):
+        """Returns all the names of the available menu items"""
+        options = ""
+        for item in self.menu:
+            options += f"{item.name}/"
+        return options[:-1]
+
+    def find_drink(self, order_name):
+        """Searches the menu for a particular drink by name. Returns that item if it exists, otherwise returns None"""
+        for item in self.menu:
+            if item.name == order_name:
+                return item
+        print("Sorry that item is not available.")
+
+    def items_list(self):
+        """Returns all the available options in the menu as a list of strings"""
+        menu_options = []
+        for item in self.menu:
+            menu_options.append(item.name)
+        return menu_options
+
